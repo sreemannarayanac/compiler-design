a2604b243443f9696bccbff0fcd6ebcc80e88833
Replit user <>
2024-02-19 17:04:07 +0000

    Added week 5 programs

---
+++ week5/sample.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int number = 10;
+    char character = 'A';
+    float decimal = 3.14;
+
+    printf("Number: %d\n", number);
+    printf("Character: %c\n", character);
+    printf("Decimal: %.2f\n", decimal);
+
+    return 0;
+}
